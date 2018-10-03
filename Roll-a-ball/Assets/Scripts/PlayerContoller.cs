925fc0c707904e0cc64fff75b147ec877356d106
unknown <azalabduljalil@gmail.com>
2018-10-03 14:25:46 -0700

    Add Roll-a-Ball project

---
+++ Roll-a-ball/Assets/Scripts/PlayerContoller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContoller : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	private int count;
+
+	public float speed;
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement*speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count += 1;
+		}
+	}
+}
