cfb941e59ba6554033900b73cdf578ad58f9eeb3
unknown <azalabduljalil@gmail.com>
2018-10-03 14:47:09 -0700

    Finish Game

--- Roll-a-ball/Assets/Scripts/PlayerContoller.cs
+++ Roll-a-ball/Assets/Scripts/PlayerContoller.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerContoller : MonoBehaviour {
 
 	private Rigidbody rb;
-
 	private int count;
 
 	public float speed;
+	public Text countText;
+	public Text WinText;
 
 	void Start(){
 		rb = GetComponent<Rigidbody> ();
 		count = 0;
+		SetCountText ();
+		WinText.text = "";
 	}
 
 	void FixedUpdate(){
@@ -28,6 +32,14 @@ public class PlayerContoller : MonoBehaviour {
 		if (other.gameObject.CompareTag ("Pick Up")) {
 			other.gameObject.SetActive (false);
 			count += 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText(){
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 7) {
+			WinText.text = "You Win";
 		}
 	}
 }
